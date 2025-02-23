c931279bca2307ece177919d51c6a3d8f331b099
Taz Jackson <tatjsn@users.noreply.github.com>
2025-02-23 11:39:20 +0000

    Use FSE

--- wp-content/themes/hello/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function my_theme_setup() {
-    add_theme_support('title-tag'); // Auto add <title>
-    register_nav_menus(['primary' => 'Main Menu']); // Enable navigation
-}
-add_action('after_setup_theme', 'my_theme_setup');
